3a1d49c79b9c8323d766fa023f502d64f31c6784
Mansi Prasad <167601533+Mansi-prasad@users.noreply.github.com>
2025-04-07 19:18:11 +0530

    Update AppDownload.jsx

--- Frontend/src/Components/AppDownload/AppDownload.jsx
+++ Frontend/src/Components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ export const AppDownload = () => {
   return (
     <div className="app-download" id="app-download">
       <p>
-        For Better Experience Download <br /> Tomato App
+        For Better Experience Download <br /> Food Hub App
       </p>
       <div className="app-download-platform">
         <img src={assets.play_store} alt="" />
